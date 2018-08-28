9b112feedb0a530fbbf9b044c9fcd097e179bd7f
Alexei Accio <alexei.accio@gmail.com>
2018-08-28 17:52:32 +0300

    cms config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,5 +2,4 @@ import CMS from "netlify-cms";
 
 import ArticlePreview from "./preview-templates/ArticlePreview";
 
-CMS.registerPreviewStyle("/styles.css");
 CMS.registerPreviewTemplate("articles", ArticlePreview);
