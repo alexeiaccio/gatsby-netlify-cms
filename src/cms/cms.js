48f8c3af99acedbd31b008c1521d49e65e4e1058
Alexei Accio <alexei.accio@gmail.com>
2018-08-28 18:24:07 +0300

    youtube widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,3 +3,28 @@ import CMS from "netlify-cms";
 import ArticlePreview from "./preview-templates/ArticlePreview";
 
 CMS.registerPreviewTemplate("articles", ArticlePreview);
+
+CMS.registerPreviewStyle("/example.css");
+
+CMS.registerEditorComponent({
+  id: "youtube",
+  label: "Youtube",
+  icon: "video",
+  fields: [{ name: "id", label: "Youtube Video ID" }],
+  pattern: /^{{<\s?youtube (\S+)\s?>}}/,
+  fromBlock: function(match) {
+    return {
+      id: match[1]
+    };
+  },
+  toBlock: function(obj) {
+    return "{{< youtube " + obj.id + " >}}";
+  },
+  toPreview: function(obj) {
+    return (
+      '<img src="http://img.youtube.com/vi/' +
+      obj.id +
+      '/maxresdefault.jpg" alt="Youtube Video"/>'
+    );
+  }
+});
