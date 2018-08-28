4c98032c09e5ca86e1bd49fe9b1df6917555e26e
Alexei Accio <alexei.accio@gmail.com>
2018-08-28 19:03:00 +0300

    utube embeding

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -11,20 +11,12 @@ CMS.registerEditorComponent({
   label: "Youtube",
   icon: "video",
   fields: [{ name: "id", label: "Youtube Video ID" }],
-  pattern: /^{{<\s?youtube (\S+)\s?>}}/,
-  fromBlock: function(match) {
-    return {
-      id: match[1]
-    };
-  },
-  toBlock: function(obj) {
-    return "{{< youtube " + obj.id + " >}}";
-  },
-  toPreview: function(obj) {
-    return (
-      '<img src="http://img.youtube.com/vi/' +
-      obj.id +
-      '/maxresdefault.jpg" alt="Youtube Video"/>'
-    );
-  }
+  pattern: /youtube\.com\/embed\/(\S+)[\?]/,
+  fromBlock: match => ({
+    id: match[1]
+  }),
+  toBlock: ({ id }) =>
+    `<iframe src="https://www.youtube.com/embed/${id}?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen width="560" height="315" ></iframe>`,
+  toPreview: ({ id }) =>
+    `<img src="https://img.youtube.com/vi/${id}/maxresdefault.jpg" alt="Youtube Video"/>`
 });
