b3fd2ac60edac3b65b0309b7be97b98bac367a20
Alexei Accio <alexei.accio@gmail.com>
2018-08-31 20:45:43 +0300

    support us

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,6 +28,8 @@ export const Footer = () => (
           'flex',
           'flex-row',
           'font-montserrat',
+          'items-center',
+          'justify-between',
           'max-w-md',
           'mx-auto',
           'px-q24',
@@ -38,22 +40,60 @@ export const Footer = () => (
     >
       <div
         className={css`
-          ${tw(['bg-contain', 'bg-no-repeat', 'pl-q24'])};
-          @media (max-width: 768px) {
-            background-image: url(${publicdomainBlack});
-          }
-          background-image: url(${publicdomain});
+          ${tw(['flex', 'items-center'])};
         `}
       >
-        ·К·Р·А·П·И·В·А·
+        <span
+          className={css`
+            ${tw([
+              'bg-contain',
+              'bg-no-repeat',
+              'h-q16',
+              'inline-block',
+              'mr-q12',
+              'w-q16',
+            ])};
+            @media (max-width: 768px) {
+              background-image: url(${publicdomainBlack});
+            }
+            background-image: url(${publicdomain});
+          `}
+        />
+        <span>·К·Р·А·П·И·В·А·</span>
       </div>
-      <div
+      <a
         className={css`
-          ${tw(['ml-auto'])};
+          ${tw([
+            'bg-green',
+            'hover:bg-black',
+            'md:hover:bg-white',
+            'border-none',
+            'font-montserrat',
+            'font-medium',
+            'inline-flex',
+            'items-center',
+            'justify-center',
+            'mx-q4',
+            'my-q4',
+            'md:my-0',
+            'outline-none',
+            'px-q24',
+            'py-q12',
+            'rounded-lg',
+            'text-black',
+            'hover:text-green',
+            'md:hover:text-black',
+            'text-md',
+            'uppercase',
+          ])};
         `}
+        href="https://yasobe.ru/na/krapiva"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        2018
-      </div>
+        Поддержать
+      </a>
+      <div>2018</div>
     </div>
   </footer>
 )
