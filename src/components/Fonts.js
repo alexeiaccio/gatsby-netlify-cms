bd53fac211f871e2ea2fee950373bcd75456bb9c
Alexei Accio <alexei.accio@gmail.com>
2018-09-04 18:21:59 +0300

    same fix

--- src/components/Fonts.js
+++ src/components/Fonts.js
@@ -8,8 +8,8 @@ export const Cormorant = css`
     font-display: swap;
     font-weight: 500;
     src: local('Cormorant Medium '), local('Cormorant-Medium'),
-      url('./static/files/Cormorant-Medium.woff2') format('woff2'),
-      url('./static/files/Cormorant-Medium.woff') format('woff');
+      url('../static/files/Cormorant-Medium.woff2') format('woff2'),
+      url('../static/files/Cormorant-Medium.woff') format('woff');
   }
 
   /* cormorant-500Italic - cyrillic */
@@ -19,8 +19,8 @@ export const Cormorant = css`
     font-display: swap;
     font-weight: 500;
     src: local('Cormorant Medium Italic'), local('Cormorant-MediumItalic'),
-      url('./static/files/Cormorant-MediumItalic.woff2') format('woff2'),
-      url('./static/files/Cormorant-MediumItalic.woff') format('woff');
+      url('../static/files/Cormorant-MediumItalic.woff2') format('woff2'),
+      url('../static/files/Cormorant-MediumItalic.woff') format('woff');
   }
 
   /* cormorant-600normal - cyrillic */
@@ -30,8 +30,8 @@ export const Cormorant = css`
     font-display: swap;
     font-weight: 600;
     src: local('Cormorant SemiBold '), local('Cormorant-SemiBold'),
-      url('./static/files/Cormorant-SemiBold.woff2') format('woff2'),
-      url('./static/files/Cormorant-SemiBold.woff') format('woff');
+      url('../static/files/Cormorant-SemiBold.woff2') format('woff2'),
+      url('../static/files/Cormorant-SemiBold.woff') format('woff');
   }
 
   /* cormorant-600Italic - cyrillic */
@@ -41,8 +41,8 @@ export const Cormorant = css`
     font-display: swap;
     font-weight: 600;
     src: local('Cormorant SemiBold Italic'), local('Cormorant-SemiBoldItalic'),
-      url('./static/files/Cormorant-SemiBoldItalic.woff2') format('woff2'),
-      url('./static/files/Cormorant-SemiBoldItalic.woff') format('woff');
+      url('../static/files/Cormorant-SemiBoldItalic.woff2') format('woff2'),
+      url('../static/files/Cormorant-SemiBoldItalic.woff') format('woff');
   }
 
   /* cormorant-700normal - cyrillic */
@@ -52,8 +52,8 @@ export const Cormorant = css`
     font-display: swap;
     font-weight: 700;
     src: local('Cormorant Bold '), local('Cormorant-Bold'),
-      url('./static/files/Cormorant-Bold.woff2') format('woff2'),
-      url('./static/files/Cormorant-Bold.woff') format('woff');
+      url('../static/files/Cormorant-Bold.woff2') format('woff2'),
+      url('../static/files/Cormorant-Bold.woff') format('woff');
   }
 
   /* cormorant-700Italic - cyrillic */
@@ -63,8 +63,8 @@ export const Cormorant = css`
     font-display: swap;
     font-weight: 700;
     src: local('Cormorant Bold Italic'), local('Cormorant-BoldItalic'),
-      url('./static/files/Cormorant-BoldItalic.woff2') format('woff2'),
-      url('./static/files/Cormorant-BoldItalic.woff') format('woff');
+      url('../static/files/Cormorant-BoldItalic.woff2') format('woff2'),
+      url('../static/files/Cormorant-BoldItalic.woff') format('woff');
   }
 `
 export const Montserrat = css`
@@ -75,8 +75,8 @@ export const Montserrat = css`
     font-display: swap;
     font-weight: 500;
     src: local('Montserrat Medium '), local('Montserrat-Medium'),
-      url('./static/files/Montserrat-Medium.woff2') format('woff2'),
-      url('./static/files/Montserrat-Medium.woff') format('woff');
+      url('../static/files/Montserrat-Medium.woff2') format('woff2'),
+      url('../static/files/Montserrat-Medium.woff') format('woff');
   }
 
   /* montserrat-500Italic - cyrillic */
@@ -86,8 +86,8 @@ export const Montserrat = css`
     font-display: swap;
     font-weight: 500;
     src: local('Montserrat Medium Italic'), local('Montserrat-MediumItalic'),
-      url('./static/files/Montserrat-MediumItalic.woff2') format('woff2'),
-      url('./static/files/Montserrat-MediumItalic.woff') format('woff');
+      url('../static/files/Montserrat-MediumItalic.woff2') format('woff2'),
+      url('../static/files/Montserrat-MediumItalic.woff') format('woff');
   }
 
   /* montserrat-600normal - cyrillic */
@@ -97,8 +97,8 @@ export const Montserrat = css`
     font-display: swap;
     font-weight: 600;
     src: local('Montserrat SemiBold '), local('Montserrat-SemiBold'),
-      url('./static/files/Montserrat-SemiBold.woff2') format('woff2'),
-      url('./static/files/Montserrat-SemiBold.woff') format('woff');
+      url('../static/files/Montserrat-SemiBold.woff2') format('woff2'),
+      url('../static/files/Montserrat-SemiBold.woff') format('woff');
   }
 
   /* montserrat-600Italic - cyrillic */
@@ -108,8 +108,8 @@ export const Montserrat = css`
     font-display: swap;
     font-weight: 600;
     src: local('Montserrat SemiBold Italic'), local('Montserrat-SemiBoldItalic'),
-      url('./static/files/Montserrat-SemiBoldItalic.woff2') format('woff2'),
-      url('./static/files/Montserrat-SemiBoldItalic.woff') format('woff');
+      url('../static/files/Montserrat-SemiBoldItalic.woff2') format('woff2'),
+      url('../static/files/Montserrat-SemiBoldItalic.woff') format('woff');
   }
 
   /* montserrat-800normal - cyrillic */
@@ -119,8 +119,8 @@ export const Montserrat = css`
     font-display: swap;
     font-weight: 800;
     src: local('Montserrat ExtraBold '), local('Montserrat-ExtraBold'),
-      url('./static/files/Montserrat-ExtraBold.woff2') format('woff2'),
-      url('./static/files/Montserrat-ExtraBold.woff') format('woff');
+      url('../static/files/Montserrat-ExtraBold.woff2') format('woff2'),
+      url('../static/files/Montserrat-ExtraBold.woff') format('woff');
   }
 
   /* montserrat-800Italic - cyrillic */
@@ -131,7 +131,7 @@ export const Montserrat = css`
     font-weight: 800;
     src: local('Montserrat ExtraBold Italic'),
       local('Montserrat-ExtraBoldItalic'),
-      url('./static/files/Montserrat-ExtraBoldItalic.woff2') format('woff2'),
-      url('./static/files/Montserrat-ExtraBoldItalic.woff') format('woff');
+      url('../static/files/Montserrat-ExtraBoldItalic.woff2') format('woff2'),
+      url('../static/files/Montserrat-ExtraBoldItalic.woff') format('woff');
   }
 `
