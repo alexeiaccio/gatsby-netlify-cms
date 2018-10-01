b7353bd6ef54dd3820c372c0b9796ccaa9d2c809
Alexei Accio <alexei.accio@gmail.com>
2018-10-01 14:48:32 +0300

    fix tipograf ru/punctuation/ano

--- src/utils/tp.js
+++ src/utils/tp.js
@@ -3,5 +3,6 @@ const Typograf = require('typograf')
 const tp = new Typograf({ locale: ['ru', 'en-US'] })
 tp.enableRule('common/html/url')
 tp.disableRule('common/nbsp/afterShortWord')
+tp.disableRule('ru/punctuation/ano')
 
 module.exports = tp
