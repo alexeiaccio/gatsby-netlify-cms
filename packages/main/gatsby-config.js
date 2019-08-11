892ded3ae91788ab85aab53f0fb7aca44b834422
Alexei Accio <alexei.accio@gmail.com>
2019-08-11 15:03:17 +0300

    main ts

--- packages/main/gatsby-config.js
+++ packages/main/gatsby-config.js
@@ -14,5 +14,13 @@ module.exports = {
         postCssPlugins: [require('autoprefixer')],
       },
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ]
 }
