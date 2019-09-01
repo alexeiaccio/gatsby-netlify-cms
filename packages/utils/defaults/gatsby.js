a5179495308ed6e573e93b7aa6ed7888b199d5f1
Alexei Accio <alexei.accio@gmail.com>
2019-09-01 21:21:58 +0300

    Update gatsby.js

--- packages/utils/defaults/gatsby.js
+++ packages/utils/defaults/gatsby.js
@@ -1,11 +1,11 @@
-const { CONFIG } = require('./config')
-const {
-  siteTitle,
-  utilsTitleShort,
-  siteThemeColor,
-  utilsBackgroundColor,
-  utilsIcon,
-} = CONFIG
+// const { CONFIG } = require('./config')
+// const {
+//   siteTitle,
+//   utilsTitleShort,
+//   siteThemeColor,
+//   utilsBackgroundColor,
+//   utilsIcon,
+// } = CONFIG
 
 exports.plugins = [
   {
@@ -31,18 +31,18 @@ exports.plugins = [
   {
     resolve: `gatsby-plugin-sitemap`,
   },
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: siteTitle,
-      short_name: utilsTitleShort,
-      start_url: '/',
-      theme_color: siteThemeColor,
-      background_color: utilsBackgroundColor,
-      display: 'minimal-ui',
-      icon: utilsIcon, // This path is relative to the root of the site.
-    },
-  },
+  // {
+  //   resolve: 'gatsby-plugin-manifest',
+  //   options: {
+  //     name: siteTitle,
+  //     short_name: utilsTitleShort,
+  //     start_url: '/',
+  //     theme_color: siteThemeColor,
+  //     background_color: utilsBackgroundColor,
+  //     display: 'minimal-ui',
+  //     icon: utilsIcon, // This path is relative to the root of the site.
+  //   },
+  // },
   {
     resolve: 'gatsby-plugin-netlify',
     options: {
