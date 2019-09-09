45c9fc9c39c68edfe55827320d3313e5db9c9061
Alexei Accio <alexei.accio@gmail.com>
2019-09-09 15:16:24 +0300

    Update footer.tsx

--- packages/theme/src/components/article-body/footer.tsx
+++ packages/theme/src/components/article-body/footer.tsx
@@ -11,7 +11,10 @@ import { footerStyles } from './styles'
 
 export function ArticleFooter() {
   const { location } = React.useContext(MetaContext)
-  const origin = get(location, 'origin', get(document, 'location.origin'))
+  const origin = (document !== undefined) ? 
+    get(document, 'location.origin')
+    :
+    get(location, 'origin', '')
   const pathname = get(location, 'pathname', '//').replace(/\/$/, '')
   const [ref, inView] = useInView({
     threshold: 0,
