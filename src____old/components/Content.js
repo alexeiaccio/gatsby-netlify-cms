7f8a779bddb486d3f6c90657c3cdb5fa0d57e226
Alexei Accio <alexei.accio@gmail.com>
2019-10-23 16:10:36 +0300

    delete old src

--- src____old/components/Content.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const HTMLContent = ({ content, className }) => (
-  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-)
-
-export const SpanHTMLContent = ({ content, className }) => (
-  <span className={className} dangerouslySetInnerHTML={{ __html: content.replace(/<\/?p/g, '<span') }} />
-)
--- src____old/utils/tp.js
+++
@@ -1,8 +0,0 @@
-const Typograf = require('typograf')
-
-const tp = new Typograf({ locale: ['ru', 'en-US'] })
-tp.enableRule('common/html/url')
-tp.disableRule('common/nbsp/afterShortWord')
-tp.disableRule('ru/punctuation/ano')
-
-module.exports = tp
